9123400954e26487537347f47dbf44bdf1909479
LOINloin <37691494+LOINloin@users.noreply.github.com>
2019-07-05 16:32:34 +0800

    Add files via upload

--- xxx.js
+++ xxx.js
@@ -1,6 +1,7 @@
 var canvases = new Array();
 canvases.push(document.getElementById("canvas"));
 canvases.push(document.getElementById("canvas2"));
+    
 
 for(var j=0;j<canvases.length;j++){ 
     
@@ -10,16 +11,16 @@ for(var j=0;j<canvases.length;j++){
     //设置是个顶点的坐标，根据顶点制定路径 
     for (var i = 0; i < 5; i++) {   
         
-        context.lineTo(Math.cos((18+i*72)/180*Math.PI)*12.5/2+10,  
-                        -Math.sin((18+i*72)/180*Math.PI)*12.5/2+32);   
+        context.lineTo(Math.cos((18+i*72)/180*Math.PI)*12.5/2+15,  
+                        -Math.sin((18+i*72)/180*Math.PI)*12.5/2+32.6);   
         /*context.lineTo(Math.cos((18 + i * 72 ) / 180 * Math.PI) * R + x,  //context：canvas画柄，r：小圆半径，R：大圆半径，x：圆心x左边，y：圆心y坐标
         -Math.sin((18 + i * 72 ) / 180 * Math.PI) * R + y);
         context.lineTo(Math.cos((54 + i * 72 ) / 180 * Math.PI) * r + x, 
         -Math.sin((54 + i * 72 ) / 180 * Math.PI) * r + y); */
 
 
-        context.lineTo(Math.cos((54+i*72)/180*Math.PI)*5/2+10,   
-                        -Math.sin((54+i*72)/180*Math.PI)*5/2+32);   
+        context.lineTo(Math.cos((54+i*72)/180*Math.PI)*5/2+15,   
+                        -Math.sin((54+i*72)/180*Math.PI)*5/2+32.6);   
 
     }   
 
